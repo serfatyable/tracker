c26ae8e66db19ee757c1cc918d9809d53dafe884
serfatyable <danieltserfaty@gmail.com>
2025-10-22 22:34:01 +0300

    chore: purge unused tab cruft and warnings in topbar/admin snapshots

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { Bars3Icon } from '@heroicons/react/24/outline';
 import dynamic from 'next/dynamic';
-import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
