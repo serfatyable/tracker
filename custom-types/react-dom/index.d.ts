6a0d068477143df283e13ef2341d3cd4e2542c87
serfatyable <113055739+serfatyable@users.noreply.github.com>
2025-11-18 13:55:12 +0200

    Inline react-dom portal declaration

--- custom-types/react-dom/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { ReactNode } from 'react';
-
-export function createPortal(
-  children: ReactNode,
-  container: Element | DocumentFragment | null,
-  key?: null | string,
-): ReactNode;
